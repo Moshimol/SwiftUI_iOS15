b0c95d6656a4ef810de0554c02fd0d26770e65c9
lushitong <lushitong@qq.com>
2022-04-17 01:30:00 +0800

    add  assent

--- Design/Design/ContentView.swift
+++ Design/Design/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+            
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.leading)
+                .foregroundColor(.secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .foregroundColor(.secondary)
+                
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.vertical, 20.0)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(color: Color("Shadow").opacity(0.4), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20.0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
     }
 }
