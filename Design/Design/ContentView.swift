6be721410889e2b258a3883fcb286c518d33aa65
lushitong <lushitong@qq.com>
2022-04-17 00:34:52 +0800

    feat: 初始化项目

---
+++ Design/Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Design
+//
+//  Created by lushitong on 2022/4/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Design/Design/DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignApp.swift
+//  Design
+//
+//  Created by lushitong on 2022/4/17.
+//
+
+import SwiftUI
+
+@main
+struct DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
