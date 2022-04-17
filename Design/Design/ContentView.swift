f2e4535e47b8faae1e4fd9370965d6229cee0b0c
lushitong <lushitong@qq.com>
2022-04-17 16:32:35 +0800

    feat：day- 05

--- Design/Design/ContentView.swift
+++ Design/Design/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in:RoundedRectangle(cornerRadius: 20, style:.continuous))
+                .strokeStyle(cornerRadius: 16)
             
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
@@ -38,9 +41,12 @@ struct ContentView: View {
         .padding(.all, 20.0)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(.ultraThinMaterial)
-        .cornerRadius(30)
+        .background(.ultraThinMaterial, in:RoundedRectangle(cornerRadius: 30, style:.continuous))
+//        .cornerRadius(30)
+//        .mask(RoundedRectangle(cornerSize: 30, style:.continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+//        .modifier(StorkeStyle())
+        .strokeStyle()
         .padding(.horizontal, 20.0)
         .background(
             Image("Blob 1")
@@ -53,6 +59,7 @@ struct ContentView: View {
                 .frame(height: 230)
                 .offset(x: 32, y: -80)
         )
+        
         
     }
 }
---
+++ Design/Design/Styles.swift
@@ -0,0 +1,33 @@
+//
+//  Styles.swift
+//  Design
+//
+//  Created by lushitong on 2022/4/17.
+//
+
+import SwiftUI
+
+struct StrokeStyle: ViewModifier {
+    var cornerRadius: CGFloat
+    @Environment(\.colorScheme) var colorScheme
+    func body(content: Content) -> some View {
+        content.overlay(
+            RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                .stroke(
+                    .linearGradient(
+                        colors: [
+                            .white.opacity(colorScheme == .dark ? 0.6 : 0.3),
+                            .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                        ], startPoint: .top, endPoint: .bottom
+                    )
+                )
+                .blendMode(.overlay)
+        )
+    }
+}
+
+extension View {
+    func strokeStyle(cornerRadius: CGFloat = 30) -> some View {
+        modifier(StrokeStyle(cornerRadius: cornerRadius))
+    }
+}
