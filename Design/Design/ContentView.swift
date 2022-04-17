3f5a0c7ca7bb71a2941960a6c2c1bdbe1ff058c1
lushitong <lushitong@qq.com>
2022-04-17 16:08:10 +0800

    fix: iOS15 later

--- Design/Design/ContentView.swift
+++ Design/Design/ContentView.swift
@@ -15,29 +15,45 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
-                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                .cornerRadius(20.0)
             
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .lineLimit(1)
+            
             Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .multilineTextAlignment(.leading)
                 .foregroundColor(.secondary)
+            
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
-                
         }
-        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
-        .padding(.vertical, 20.0)
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(Color("Background"))
-        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
-        .shadow(color: Color("Shadow").opacity(0.4), radius: 10, x: 0, y: 10)
+        .background(.ultraThinMaterial)
+        .cornerRadius(30)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .padding(.horizontal, 20.0)
+        .background(
+            Image("Blob 1")
+                .offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
+        
     }
 }
 
