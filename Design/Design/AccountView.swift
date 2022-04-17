5a3834af9fad2cc399cd36246f4bb034fa041694
lushitong <lushitong@qq.com>
2022-04-17 23:05:41 +0800

    feat: profile add animation

--- Design/Design/AccountView.swift
+++ Design/Design/AccountView.swift
@@ -39,6 +39,11 @@ struct AccountView: View {
                     HexagonView()
                         .offset(x: -50, y: -100)
                 )
+                .background(
+                    BlobView()
+                        .offset(x: 200, y: 0)
+                        .scaleEffect(0.6)
+                )
             Text("Lu Shitong")
                 .font(.title.weight(.semibold))
             HStack {
